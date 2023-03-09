c4d326d8b87923de4a2f952ea3c54cbabdc2df43
Elmouden1EZ1 <hafidaelmouden68@gmail.com>
2023-03-09 21:28:32 +0000

    First commit

---
+++ test.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+
+# Créer une instance du navigateur Chrome
+driver = webdriver.Chrome()
+
+# Ouvrir l'URL de Google dans le navigateur
+driver.get("https://www.google.com")
